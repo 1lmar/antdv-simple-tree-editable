6b41bbe3cdd42da8532bb009d7e37b93787e90a0
Ilmar Shakirov <i.shakirov@infomatika.ru>
2022-04-19 22:17:27 +0300

    refactor: add hook tests

--- tests/hooks.test.ts
+++ tests/hooks.test.ts
@@ -2,8 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { useTreeDataEditable } from "../src/hooks/useTreeDataEditable"
 
 describe('useTreeDataEditable', () => {
-  it('should return a valid TreeData array', () => {
-    const { createNodes, getTreeData } = useTreeDataEditable<string>()
+    const { createNodes, getTreeData, getTreeItem, getParentKey, createParentNode } = useTreeDataEditable<string>()
 
     createNodes([
         {
@@ -19,22 +18,54 @@ describe('useTreeDataEditable', () => {
         },
     ])
 
-    const expectedTreeData = [
-        {
-            key: '0-0',
-            title: 'parent-0',
-            VALUE: 'value',
-            children: [
-                {
-                    key: '0-0-0',
-                    title: 'child-0-0',
-                    VALUE: 'value',
-                    children: [],
-                },
-            ],
-        }
-    ]
+    it('should return a valid TreeData array', () => {
+        const expectedTreeData = [
+            {
+                key: '0-0',
+                title: 'parent-0',
+                VALUE: 'value',
+                children: [
+                    {
+                        key: '0-0-0',
+                        title: 'child-0-0',
+                        VALUE: 'value',
+                        children: [],
+                    },
+                ],
+            }
+        ]
 
-    expect(getTreeData.value).toEqual(expectedTreeData)
-  })
+        expect(getTreeData.value).toEqual(expectedTreeData)
+    })
+
+    it('should return a valid tree item', () => {
+        const expectedTreeItem = {
+            KEY: '0-0-0',
+            TITLE: 'child-0-0',
+            VALUE: 'value',
+            CHILDS: [],
+        }
+
+        expect(getTreeItem('0-0-0')).toEqual(expectedTreeItem)
+    })
+
+    it('should return a parent key', () => {
+        expect(getParentKey('0-0-0')).toEqual('0-0')
+    })
+
+    it('should create a parent node', () => {
+        const expectedParentNode = {
+            KEY: '0-1',
+            TITLE: 'new parent node',
+            VALUE: 'value',
+            CHILDS: [],
+        }
+
+        createParentNode({
+            TITLE: expectedParentNode.TITLE,
+            VALUE: expectedParentNode.VALUE
+        })
+
+        expect(getTreeItem(expectedParentNode.KEY)).toEqual(expectedParentNode)
+    })
 })
